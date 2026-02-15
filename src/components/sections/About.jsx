15564f4ff654e80dace1385c27f26457722de067
mathieumontanino-wq <mathieumontanino@gmail.com>
2026-02-15 16:58:01 +0100

    Finalisation page Services : intégration banner, ajustement traits, correction hover cards, mise à jour About

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,80 +2,123 @@ import aboutImg from "../../assets/images/john-doe-about.jpg";
 
 function About() {
   return (
-    <section className="py-5 bg-light">
+    <section className="about-section py-5">
       <div className="container">
-        <div className="card shadow p-4">
+        <div className="about-card p-5 shadow-sm">
 
           <div className="row">
 
             {/* Colonne gauche */}
-            <div className="col-md-6">
-              <h4 className="fw-bold border-bottom pb-2 mb-4">
-                A propos
-              </h4>
+            <div className="col-12 col-lg-6">
+
+              <h4 className="section-title">A propos</h4>
+              <div className="section-line"></div>
 
               <img
                 src={aboutImg}
-                alt="Photo de John Doe en train de travailler"
-                className="img-fluid rounded mb-3"
+                alt="John Doe en train de travailler"
+                className="about-img mb-4"
               />
 
               <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at purus euismod, tempus metus luctus, pharetra nulla. Maecenas mattis elit.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nulla at purus euismod, tempus metus luctus, pharetra nulla.
+                Maecenas mattis elit.
               </p>
+
               <p>
-               Pellentesque facilisis ultricies sapien, non finibus tortor porttitor ac. Nam a vulputate lorem. Nam ut mauris faucibus, commodo justo nec.
+                Pellentesque facilisis ultricies sapien, non finibus tortor
+                porttitor ac. Nam a vulputate lorem. Nam ut mauris faucibus,
+                commodo justo nec.
               </p>
+
               <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at purus euismod, tempus metus luctus, pharetra nulla. Maecenas mattis elit.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nulla at purus euismod, tempus metus luctus, pharetra nulla.
+                Maecenas mattis elit.
               </p>
+
             </div>
 
             {/* Colonne droite */}
-            <div className="col-md-6">
-              <h4 className="fw-bold border-bottom pb-2 mb-4">
-                Mes compétences
-              </h4>
+            <div className="col-12 col-lg-6">
 
-              <div className="mb-3">
-                <span>HTML5 90%</span>
-                <div className="progress">
-                  <div className="progress-bar bg-danger" style={{ width: "90%" }}></div>
+              <h4 className="section-title">Mes compétences</h4>
+              <div className="section-line"></div>
+
+              {/* HTML */}
+              <div className="mb-4">
+                <div className="d-flex justify-content-between">
+                  <span>HTML5</span>
+                  <span>90%</span>
+                </div>
+                <div className="progress mt-2">
+                  <div
+                    className="progress-bar bg-danger"
+                    style={{ width: "90%" }}
+                  ></div>
                 </div>
               </div>
 
-              <div className="mb-3">
-                <span>CSS3 80%</span>
-                <div className="progress">
-                  <div className="progress-bar bg-info" style={{ width: "80%" }}></div>
+              {/* CSS */}
+              <div className="mb-4">
+                <div className="d-flex justify-content-between">
+                  <span>CSS3</span>
+                  <span>80%</span>
+                </div>
+                <div className="progress mt-2">
+                  <div
+                    className="progress-bar bg-info"
+                    style={{ width: "80%" }}
+                  ></div>
                 </div>
               </div>
 
-              <div className="mb-3">
-                <span>JAVASCRIPT 70%</span>
-                <div className="progress">
-                  <div className="progress-bar bg-warning" style={{ width: "70%" }}></div>
+              {/* JS */}
+              <div className="mb-4">
+                <div className="d-flex justify-content-between">
+                  <span>JAVASCRIPT</span>
+                  <span>70%</span>
+                </div>
+                <div className="progress mt-2">
+                  <div
+                    className="progress-bar bg-warning"
+                    style={{ width: "70%" }}
+                  ></div>
                 </div>
               </div>
 
-              <div className="mb-3">
-                <span>PHP 60%</span>
-                <div className="progress">
-                  <div className="progress-bar bg-success" style={{ width: "60%" }}></div>
+              {/* PHP */}
+              <div className="mb-4">
+                <div className="d-flex justify-content-between">
+                  <span>PHP</span>
+                  <span>60%</span>
+                </div>
+                <div className="progress mt-2">
+                  <div
+                    className="progress-bar bg-success"
+                    style={{ width: "60%" }}
+                  ></div>
                 </div>
               </div>
 
-              <div className="mb-3">
-                <span>REACT 50%</span>
-                <div className="progress">
-                  <div className="progress-bar bg-primary" style={{ width: "50%" }}></div>
+              {/* React */}
+              <div className="mb-4">
+                <div className="d-flex justify-content-between">
+                  <span>REACT</span>
+                  <span>50%</span>
+                </div>
+                <div className="progress mt-2">
+                  <div
+                    className="progress-bar bg-primary"
+                    style={{ width: "50%" }}
+                  ></div>
                 </div>
               </div>
 
             </div>
 
           </div>
-
         </div>
       </div>
     </section>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,80 @@
+import banner from "../assets/images/banner.jpg";
+import { FaPaintBrush, FaCode, FaSearch } from "react-icons/fa";
+
 function Services() {
   return (
-    <div className="container mt-5">
-      <h1 className="text-center">Services</h1>
-    </div>
+    <>
+      {/* Bannière */}
+      <section
+        className="services-banner"
+        style={{ backgroundImage: `url(${banner})` }}
+      ></section>
+
+      {/* Section services */}
+      <section className="services-section py-5">
+        <div className="container">
+
+          {/* Titre */}
+          <div className="text-center mb-5">
+            <h1 className="services-title">Mon offre de services</h1>
+            <p className="services-subtitle">
+              Voici les prestations sur lesquelles je peux intervenir
+            </p>
+            <div className="services-divider"></div>
+          </div>
+
+          {/* Cards */}
+          <div className="row g-4">
+
+            {/* UX Design */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card service-card h-100 text-center p-4">
+                <FaPaintBrush className="service-icon mb-3" />
+                <h3 className="mb-3">UX Design</h3>
+                <p>
+                  L'UX Design est une discipline qui consiste à concevoir des
+                  produits (sites web, applications mobiles, logiciels, objets
+                  connectés, etc.) en plaçant l'utilisateur au centre des
+                  préoccupations. L'objectif est de rendre l'expérience
+                  utilisateur la plus fluide et agréable possible.
+                </p>
+              </div>
+            </div>
+
+            {/* Développement */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card service-card h-100 text-center p-4">
+                <FaCode className="service-icon mb-3" />
+                <h3 className="mb-3">Développement web</h3>
+                <p>
+                  Le développement de sites web consiste à créer des sites
+                  internet en utilisant des langages de programmation (HTML,
+                  CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap,
+                  React, Angular, etc.).
+                </p>
+              </div>
+            </div>
+
+            {/* SEO */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card service-card h-100 text-center p-4">
+                <FaSearch className="service-icon" />
+
+                <h3 className="mb-3">Référencement</h3>
+                <p>
+                  Le référencement naturel (SEO) est une technique qui consiste
+                  à optimiser un site web pour le faire remonter dans les
+                  résultats des moteurs de recherche (Google, Bing, Yahoo,
+                  etc.). L'objectif est d'attirer un maximum de visiteurs
+                  qualifiés sur le site.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
